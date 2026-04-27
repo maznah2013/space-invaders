fe204637511a9c8c4883d6dec0cc2f10b140b58d
maznah2013 <maznah201357@gmail.com>
2026-04-27 16:51:47 +0400

    Add files via upload

--- game.py
+++ game.py
@@ -36,3 +36,51 @@ def draw_window(red,yellow,red_bullets,yellow_bullets,red_lives,yellow_lives):
     screen.blit(red_lives_text,(WIDTH-red_lives_text.get_width()-10,10))
     yellow_lives_text=LIVES_FONT.render(f"Lives: {yellow_lives}",1,"white")
     screen.blit(yellow_lives_text,(10,10))
+
+    screen.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    screen.blit(RED_SPACESHIP, (red.x, red.y))
+
+    for bullet in yellow_bullets:
+        pygame.draw.rect(screen, "yellow", bullet)
+
+    for bullet in red_bullets:
+        pygame.draw.rect(screen, "red", bullet)
+
+    pygame.display.update()
+
+
+def yellow_handle_movement(keys_pressed, yellow):
+
+    if keys_pressed[pygame.K_a] and yellow.x-VEL>0:
+        yellow.x-=VEL
+
+    if keys_pressed[pygame.K_d] and yellow.x+VEL+yellow.width<BORDER.x:
+        yellow.x+=VEL
+
+    if keys_pressed[pygame.K_w] and yellow.y-VEL>0:
+        yellow.y-=VEL
+
+    if keys_pressed[pygame.K_d] and yellow.y+VEL+yellow.height<HEIGHT-15:
+        yellow.y+=VEL
+
+def red_handle_movement(keys_pressed, red):
+     
+    if keys_pressed[pygame.K_LEFT] and red.x-VEL>BORDER.x+BORDER.width:
+        red.x-=VEL
+
+    if keys_pressed[pygame.K_RIGHT] and red.x+VEL+red.width<WIDTH:
+        red.x+=VEL
+
+    if keys_pressed[pygame.K_UP] and red.y-VEL>0:
+        red.y-=VEL
+
+    if keys_pressed[pygame.K_DOWN] and red.y+VEL+red.height<HEIGHT-15:
+        red.y+=VEL
+
+YELLOW_HIT=pygame.USEREVENT+1 
+RED_HIT=pygame.USEREVENT+2  
+
+def handle_bullets(yellow_bullets, red_bullets, yellow, red):
+    for bullet in yellow_bullets:
+        bullet.x+=VEL
+        
